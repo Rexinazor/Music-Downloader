cb3d12dbc6b6ecaaf051bb026ff78953f042916c
ʀᴇxɪɴᴀᴢᴏʀ <85327329+Rexinazor@users.noreply.github.com>
2022-03-26 15:22:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config:
     API_HASH = os.environ.get("API_HASH", "")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
     START_MSG = os.environ.get("START_MSG", "<b>Hi {},\nI am Music Downloader Bot,</b>\n\nSend me Any Songs name directly or with /song command")
-    START_IMG = os.environ.get("START_IMG", "https://telegra.ph/file/2a35fca576aa49de77c98.jpg")
+    START_IMG = os.environ.get("START_IMG", "https://telegra.ph/file/1579bfa1c8394f308df1e.png")
     OWNER = os.environ.get("OWNER", "ZenoByte") 
     DOWNLOAD_LOCATION = os.environ.get("DOWNLOAD_LOCATION", "./DOWNLOADS/")
     msg = {}
